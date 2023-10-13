973458e881c895d44b6702fe81ccda579cd996b4
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-10-13 10:48:02 +0300

    Add files via upload

--- NaidisForm/MyForm.cs
+++ NaidisForm/MyForm.cs
@@ -1,23 +1,81 @@
-﻿using System.ComponentModel;
-using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace NaidisForm
 {
     public partial class MyForm : Form
     {
+        private TrianglePictureBoxControl trianglePictureBox;
+        private Label heightLabel;
+        private Label medianLabel;
+        private TextBox sideATextBox;
+        private TextBox sideBTextBox;
+        private TextBox sideCTextBox;
+
+        private void CalculateTriangle(object sender, EventArgs e)
+        {
+            double sideA, sideB, sideC;
+            if (double.TryParse(sideATextBox.Text, out sideA) &&
+                double.TryParse(sideBTextBox.Text, out sideB) &&
+                double.TryParse(sideCTextBox.Text, out sideC))
+            {
+                Triangle triangle = new Triangle(sideA, sideB, sideC);
+                double height = triangle.Height();
+                double median = triangle.Median();
+
+                heightLabel.Text = $"Высота: {height}";
+                medianLabel.Text = $"Медиана: {median}";
+
+                trianglePictureBox.Invalidate();
+            }
+        }
+        public MyForm()
+        {
+            InitializeComponent();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+
+            trianglePictureBox = new TrianglePictureBoxControl();
+            heightLabel = new Label();
+            medianLabel = new Label();
+            sideATextBox = new TextBox();
+            sideBTextBox = new TextBox();
+            sideCTextBox = new TextBox();
+
+            trianglePictureBox.Location = new Point(10, 10);
+            trianglePictureBox.Size = new Size(200, 200);
+            heightLabel.Location = new Point(10, 220);
+            medianLabel.Location = new Point(10, 240);
+            sideATextBox.Location = new Point(10, 260);
+            sideBTextBox.Location = new Point(10, 280);
+            sideCTextBox.Location = new Point(10, 300);
+
+            this.Controls.Add(trianglePictureBox);
+            this.Controls.Add(heightLabel);
+            this.Controls.Add(medianLabel);
+            this.Controls.Add(sideATextBox);
+            this.Controls.Add(sideBTextBox);
+            this.Controls.Add(sideCTextBox);
+
+            sideATextBox.TextChanged += CalculateTriangle;
+            sideBTextBox.TextChanged += CalculateTriangle;
+            sideCTextBox.TextChanged += CalculateTriangle;
+        }
         class Triangle
         {
+
             public double a;
             public double b;
             public double c;
             public double height;
+            public double median;
             public Triangle(double A, double B, double C)
             {
                 a = A;
                 b = B;
                 c = C;
             }
-           
+
             public string outputA()
             {
                 return Convert.ToString(a);
@@ -44,12 +102,18 @@ namespace NaidisForm
                 s= Math.Sqrt((p * (p - a) + (p - b) + (p - c)));
                 return s;
             }
-            public double CalculateHeight()
+            public double Height()
             {
                 double s = Surface();
                 double height = 2 * s / a;
                 return height;
             }
+            public double Median()
+            {
+                median = 0.5 * a;
+                return median;
+            }
+
             public double GetSetA
             {
                 get
---
+++ NaidisForm/TrianglePictureBoxControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NaidisForm
+{
+    public partial class TrianglePictureBoxControl : PictureBox
+    {
+        private double sideA = 3;
+        private double sideB = 4;
+        private double sideC = 5;
+
+        public TrianglePictureBoxControl()
+        {
+            InitializeComponent();
+        }
+
+        public void SetTriangleSides(double a, double b, double c)
+        {
+            sideA = a;
+            sideB = b;
+            sideC = c;
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Black);
+
+            double p1x = 50;
+            double p1y = 150;
+            double p2x = p1x + sideA * 10;
+            double p2y = p1y;
+            double heightX = p1x + (sideA / 2) * 10; 
+            double heightY = p1y - (Math.Sqrt(sideB * sideB - (sideA / 2) * (sideA / 2))) * 10;
+
+            Point p1 = new Point((int)p1x, (int)p1y);
+            Point p2 = new Point((int)p2x, (int)p2y);
+            Point p3 = new Point((int)heightX, (int)heightY);
+
+            g.DrawLine(pen, p1, p2);
+            g.DrawLine(pen, p2, p3);
+            g.DrawLine(pen, p3, p1);
+        }
+    }
+
+}
